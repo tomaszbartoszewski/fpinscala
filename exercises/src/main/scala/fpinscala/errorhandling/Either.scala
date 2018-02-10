45aa886674260f581176e96641bfcc4b6c9865ca
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2018-02-10 20:59:20 +0000

    Exercise 4.6, Either map, flatMap, orElse, map2 implementation

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,13 +4,35 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+  // (Right[Int](15): Either[String, Int]).map(_ * 2)
+  // (Left[String]("Error"): Either[String, Int]).map(_ * 2)
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(r) => Right(f(r))
+  }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  // (Right[Int](15): Either[String, Int]).flatMap(x => Left[String]("Test"))
+  // (Right[Int](15): Either[String, Int]).flatMap(x => Right[Int](x * 2))
+  // (Left[String]("Test"): Either[String, Int]).flatMap(x => Right[Int](x * 2))
+  // (Left[String]("Test"): Either[String, Int]).flatMap(x => Left[String]("Other"))
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(r) => f(r)
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  // (Right(15)).orElse(Right(30))
+  // (Left("Error")).orElse(Right(30))
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(r) => Right(r)
+  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  // Right(15).map2(Right(4))(_ * _)
+  // Right(15).map2(Left("Error"): Either[String, Int])(_ * _)
+  // (Left("Test"): Either[String, Int]).map2(Left("Error"): Either[String, Int])(_ * _)
+  // (Left("Test"): Either[String, Int]).map2(Right(15): Either[String, Int])(_ * _)
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    this flatMap(a => b map (bb => f(a, bb)))
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
